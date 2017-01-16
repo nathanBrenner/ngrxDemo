a694f54bc3db44e644542694b27467b1ae914d47
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-15 18:55:20 -0800

    added ng material, routing, and additional components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 // ngrx
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -14,9 +16,24 @@ import { sessionReducer } from './shared/reducers/session.reducer';
 // root component
 import { AppComponent } from './app.component';
 
+// routing
+import { AppRoutingModule } from './app.routing';
+
+// dumb components
+import { RegistrationComponent } from './registration/registration.component';
+
+// smart components
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     StoreModule.provideStore({
@@ -25,7 +42,9 @@ import { AppComponent } from './app.component';
     StoreDevtoolsModule.instrumentOnlyWithExtension(),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
