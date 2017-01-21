4a6aea207c902e53a50d04e1f52edb0050d597df
nathan-j-brenner <nathan.j.brenner@gmail.com>
2017-01-21 14:17:04 -0800

    added session persistence and logout action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,40 @@
-import { Component } from '@angular/core';
-import { Store } from '@ngrx/store'
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 
+import { Session } from './shared/models/session.interface';
+import { SESSION_ACTIONS } from './shared/reducers/session.reducer';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   session$;
+
+  checkLocalStorage(){
+    if (localStorage['Authorization']) return this.getUser();
+  }
 
   constructor(private _store: Store<any>){
     this.session$ = _store.select('session');
   }
 
-  onSignOut(){
-    // todo
+  getUser(){
+    this._store.dispatch({
+      type: SESSION_ACTIONS.GET_USER.ATTEMPT,
+      payload: {}
+    });
   }
 
+  ngOnInit(){
+    this.checkLocalStorage();
+  }
+
+  onSignOut(){
+    this._store.dispatch({
+      type: SESSION_ACTIONS.LOGOUT_USER.ATTEMPT,
+      payload: {}
+    });
+  }
 }
